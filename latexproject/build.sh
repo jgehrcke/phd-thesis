793cf9fafa542c2dad87c0596b582169249dead0
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-08-13 17:49:15 +0200

    Implement nomenclature index system.

--- latexproject/build.sh
+++ latexproject/build.sh
@@ -2,6 +2,7 @@
 
 biber main
 lualatex --interaction=nonstopmode --output-format=pdf main.tex
+makeindex -s nomencl.ist -t main.nlg -o main.nls main.nlo
 
 #if [ $? -eq 0 ]; then
 #    okular main.pdf
